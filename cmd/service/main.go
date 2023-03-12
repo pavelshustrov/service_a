2d8ffb175bee156a27f0957cacd33c9fa2df0d6d
Pavel Shustrov <pashustrov@avito.ru>
2023-03-12 23:36:16 +0100

    init

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+
 	v1 "github.com/pavelshustrov/service_b/pkg/client/v1"
 )
 
 func main() {
 	fmt.Println("service A started")
-	v1.New()
+	client := v1.New()
+	id, err := client.Create(context.Background())
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	log.Println(id.UUID)
 }
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com:pavelshustrov/service_a
-
-go 1.19
